dace4cac386645513caf33323f73bdfff887911f
cindyferreira <licindyfecu@hotmail.com>
2022-09-21 01:50:40 -0300

    I create the function for the Tic-Tac-Toe game

---
+++ tic-tac-toe.py
@@ -0,0 +1,34 @@
+
+def main():
+    
+    return
+
+def game_introduction():
+    ###
+    # welcome to the game and assign the players
+    ###
+    
+    return  
+    
+def play(player):
+    ### 
+    # movement record
+    # ###
+    
+    return 
+
+def verify_status(position): 
+    ###
+    # verify if there is a winner
+    # ###
+    
+    return
+
+def final_message(result): 
+   ### 
+   # when the game end, print a message with the result
+   # ### 
+    
+    return
+    
+main()
