45bd666579bab80a0d2a2c156b26e3120d698d1a
cindyferreira <licindyfecu@hotmail.com>
2022-09-22 00:36:11 -0300

    I wrote the welcome and registered the player's names.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,14 +1,21 @@
 
 def main():
+    players = game_introduction()
     
-    return
 
 def game_introduction():
     ###
     # welcome to the game and assign the players
     ###
     
-    return  
+    print("\n================ Welcome to Tic-Tac-Toe game ========================\nTic-Tac-Toe is a game in which two players seek in alternate\nturns to complete a row, a column, or a diagonal with either\nthree x's or three o's drawn in the spaces of a grid of nine squares.\nThe first player to get three of their marks in a row is the winner.\n=====================================================================\n")
+    
+    players = ["",""]
+    
+    players[0] = input("Enter the name of the player one: ")
+    players[1] = input("Enter the name of the player two: ")
+    
+    return players
     
 def play(player):
     ### 
